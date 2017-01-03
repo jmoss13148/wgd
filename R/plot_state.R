205091c9e8f3071d615b77359d448606734df070
jmoss13148 <jm21@williams.edu>
2017-01-03 14:57:38 -0500

    Add files via upload

---
+++ R/plot_state.R
@@ -0,0 +1,62 @@
+#' @title Plot State
+#' @description Uses the geographic distribution data from the readEphData function to generate graphics for user coded states.
+#' @param region The region that will be plotted. The possible options are West, Mountain West, South, Midwest, Northeast, Mid Atlantic, and All.
+#' They should be coded as strings without spaces and with capital letter where normal. For example, /code{"NorthCarolina"}
+#' @return A barplot for the specified region. If the user codes /code{"Notable"}, the function will return notable states.
+#' @usage
+#' plot_state(state)
+#' @export
+
+# enter state or province without spaces and with capital letters where normal
+plot_state <- function(state) {
+
+  williams_data <- cbind(readEphData("williams2001.txt"), readEphData("williams2002.txt"), readEphData("williams2003.txt"), readEphData("williams2004.txt"), readEphData("williams2005.txt"), readEphData("williams2006.txt"), readEphData("williams2007.txt"), readEphData("williams2008.txt"), readEphData("williams2009.txt"), readEphData("williams2010.txt"), readEphData("williams2011.txt"), readEphData("williams2012.txt"), readEphData("williams2013.txt"), readEphData("williams2014.txt"), readEphData("williams2015.txt"), readEphData("williams2016.txt"))
+  williams_data <- williams_data[, c(ncol(williams_data) - 1, seq(2, 32, by = 2))] # columns of interest
+  x <- as.data.frame(lapply(williams_data[, 2:ncol(williams_data)], as.numeric)) # make columns of interest numeric
+  dat <- as.data.frame(cbind(williams_data[, 1], x)) # combine these with list of states
+  names(dat) <- c("State", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011",
+                  "2012", "2013", "2014", "2015", "2016")
+
+  if(state %in% dat[, 1]) {
+  data <- dat[dat$State == state, ] #subset for state of interest
+  data <- as.numeric(data[, 2:ncol(data)])
+      names(data) <- c("2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011",
+                       "2012", "2013", "2014", "2015", "2016")
+  df.bar <- barplot(data, main = state, bg = "grey", col = "purple", ylab = "Number of Students")
+  lines(x = df.bar, y = data/2, col = "white")
+  return(points(x = df.bar, y = data/2, col = "white"))
+  }
+  else if(state == "Notable") {
+    data <- as.matrix(dat[dat$State %in% c("California", "Florida", "Hawaii", "Massachusetts", "Maine"), 2:ncol(data)])
+        df.bar <- plot(data[1, ], ylim = c(0, 450), main = "Notable States", xlab = "Year", ylab = "Number of Students", axes = FALSE, bty = "L")
+        v1 <- c(1, 6, 11, 16) # -> defines position of tick marks.
+        v2 <- c("2001", "2006", "2011", "2016") # defines labels of tick marks.
+    axis(side = 1, at = v1, labels = v2, tck = -.05)
+    axis(side = 2)
+        lines(data[1, ], type = "o", pch = 15, lty=2, col = "red")
+        lines(data[2, ], type = "o", pch = 16, lty=2, col = "black")
+        lines(data[3, ], type = "o", pch = 17, lty=2, col = "purple")
+        lines(data[4, ], type = "o", pch = 18, lty=2, col = "green")
+        lines(data[5, ], type = "o", pch = 16, lty=2, col = "blue")
+    colors = c("red", "black", "purple", "green", "blue")
+    par(xpd=TRUE)
+    legend(x = 10 , y = 525 , c("California", "Florida", "Hawaii", "Maine", "Massachusetts"), bty ="n", fill = colors, cex = 0.6)
+  } else { stop("Input any valid state or Notable. Example: plot_region('DistrictofColumbia')") }
+}
+
+# suggested states
+# plot_state("California")
+# plot_state("DistrictofColumbia")
+# plot_state("Florida")
+# plot_state("Hawaii")
+# plot_state("Guam")
+# plot_state("Maine")
+# plot_state("Massachusetts")
+# plot_state("Mississippi")
+# plot_state("Montana")
+# plot_state("NewHampshire")
+# plot_state("PuertoRico")
+# plot_state("Texas")
+# plot_state("Vermont")
+# plot_state("Virginia")
+
